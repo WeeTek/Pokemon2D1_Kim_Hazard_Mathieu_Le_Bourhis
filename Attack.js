7416de9a4235741c80aef384907e70104623b533
WeeTek <62064251+WeeTek@users.noreply.github.com>
2024-03-15 17:43:34 +0100

    correction du fichier Attacks.js
    -ajout de commentaire
    -meilleur affichage du toString

--- Attack.js
+++ Attack.js
@@ -1,14 +1,32 @@
 class Attack {
+
+    /* Constructeur de l'Objet Attack
+    Prends en paramètres:
+    -durée
+    -énergie consommé
+    -id du mouvement
+    -nom du mouvement
+    -puissance
+    -endurance perdu
+    -chance critique
+    -endurance perdu
+    */ 
     constructor(duration, energy_delta, move_id, name, power, stamina_loss_scaler, critical_chance=null) {
-        this.duration = duration;
-        this.energy_delta = energy_delta;
-        this.id = move_id;
-        this.name = name;
-        this.critical_chance = critical_chance;
-        this.power = power;
-        this.stamina_loss_scaler = stamina_loss_scaler;
+        this.duration = duration; //durée de l'action
+        this.energy_delta = energy_delta; //énergie consommé
+        this.id = move_id; // id de l'attaque
+        this.name = name; //nom de 
+        this.critical_chance = critical_chance; //chance de coup critique
+        this.power = power; //puissance de l'attaque
+        this.stamina_loss_scaler = stamina_loss_scaler; //endurance perdu
     }
     toString = () => {
-        return `${this.id} - ${this.name} - ${this.duration} - ${this.energy_delta} - ${this.power} - ${this.stamina_loss_scaler} - ${this.critical_chance}`;
+        print("Durée de l'action : ", this.duration);
+        print("Nom de l'action: ", this.name); 
+        print("Nom Pokemon: ", this.critical_chance);
+        print("Attaque: ", this.power); 
+        print("Defense: ", this.move_id); 
+        print("Energie: ", this.energy_delta);
+        print("Generation: ", this.stamina_loss_scaler);
     }
 }
